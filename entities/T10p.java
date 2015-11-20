e52127a01fe541968d91a844fe1cbe7dceb71c99
marinadaschenko1 <marinadaschenko1@gmail.com>
2015-11-20 18:58:36 +0300

    Update T10p.java

--- entities/T10p.java
+++ entities/T10p.java
@@ -18,13 +18,31 @@ public class T10p {
         this.fp = p;
         this.fa = a;
     }
-
-
-
-    public void setP(int p) {
+    
+    public String doTransfer(float aArg) 
+    {
+         String sen = "";
+        if(aArg < 0) {
+            sen = "-" + int10ToP((int) -aArg);
+            sen += ",";
+            sen += frac10ToP(-aArg);
+        } else {
+            sen = int10ToP((int) aArg);
+            sen += ",";
+            sen += frac10ToP(aArg);
+        }
+        return sen.charAt(sen.length()-1) == ',' ? sen.substring(0, sen.length()-1) : sen;
+    }
+    
+   public void setP(int p) {
         if (p < 2 && p > TP_SYMBOLS.length())
             throw new IllegalArgumentException("Основание системы счисления должно быть от 2 до " + TP_SYMBOLS.length());
         this.fp = p;
+    }
+    
+     public int getP() 
+     {
+        
     }
 
 
