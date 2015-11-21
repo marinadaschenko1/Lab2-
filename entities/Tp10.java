63c87ff86ee8628c0104957759422715ab7c962d
marinadaschenko1 <marinadaschenko1@gmail.com>
2015-11-21 14:31:45 +0300

    Update Tp10.java

--- entities/Tp10.java
+++ entities/Tp10.java
@@ -15,6 +15,69 @@ public class Tp10 {
             throw new IllegalArgumentException("Основание системы счисления должно быть от 2 до " + TP_SYMBOLS.length());
         this.fp = p;
     }
+    
+       public float doTransfer(String aArg) {
+        float result = 0;
+        String[] number;
+        String leftString;
+        String rightString;
+
+        number = aArg.split(",");
+
+        try {
+            leftString = number[0];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            leftString = aArg;
+        }
+        try {
+            rightString = number[1];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            rightString = aArg;
+        }
+
+        if(leftString.charAt(0) == '-') {
+            result += pIntTo10(leftString.substring(1));
+            result += pFracTo10(rightString);
+            result *= -1;
+        } else {
+            result += pIntTo10(leftString);
+            result += pFracTo10(rightString);
+        }
+
+        return  ;
+    }
+    
+    public float doTransfer(String aArg) 
+    {
+        float result = 0;
+        String[] number;
+        String leftString;
+        String rightString;
+
+        number = aArg.split(",");
+
+        try {
+            leftString = number[0];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            leftString = aArg;
+        }
+        try {
+            rightString = number[1];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            rightString = "";
+        }
+
+        if(leftString.charAt(0) == '-') {
+            result += pIntTo10(leftString.substring(1));
+            result += pFracTo10(rightString);
+            result *= -1;
+        } else {
+            result += pIntTo10(leftString);
+            result += pFracTo10(rightString);
+        }
+
+        return  ;
+    }
 
 
     public void setP(int p) {
